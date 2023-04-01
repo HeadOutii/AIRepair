a25f6dd1869458ddea4c2328802fd3b1438923ba
HeadOutii <mr.pancake456781@gmail.com>
2023-04-01 17:51:09 +0300

    add display, icon, name

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hi")
+import pygame
+
+pygame.init()
+
+display_w = 800
+display_h = 600
+
+display = pygame.display.set_mode((display_w, display_h))
+pygame.display.set_caption('Forgotten Warrior')
+
+icon = pygame.image.load('icons.png')
+pygame.display.set_icon(icon)
+
+
+def run_game():
+    game = True
+
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        display.fill((255,255,255))
+        pygame.display.update()
+
+run_game()
